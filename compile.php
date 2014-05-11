a48b1899cff2110465caa9f158f48a0abd0f1782
Yuji ODA <yuji.od@gmail.com>
2014-05-11 22:44:20 +0900

    Modified for UglifyJS 2.

--- compile.php
+++ compile.php
@@ -4,6 +4,6 @@
 $css = trim(shell_exec('recess --compress ./Source/style.less'));
 $logo = base64_encode(file_get_contents('./Source/logo.png'));
 $css = str_replace('url(logo.png)', 'url(data:image/png;base64,'.$logo.')', $css);
-$script = trim(shell_exec('coffee -p -c ./Source/script.coffee  | uglifyjs -nc'));
+$script = trim(shell_exec('coffee -p -c ./Source/script.coffee  | uglifyjs -m'));
 $script = str_replace('<!--%css%-->', $css, $script);
 echo $script;
